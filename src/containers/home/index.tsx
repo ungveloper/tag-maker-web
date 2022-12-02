3ee921b28b12f9e16a9fc94add2523c6b9ddb899
ungveloper <106213837+ungveloper@users.noreply.github.com>
2022-12-02 17:08:47 +0900

    react-fast-compare 추가

--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -1,5 +1,8 @@
+import { memo } from "react";
+import isEqual from "react-fast-compare";
+
 function Home(): JSX.Element {
   return <h1>Hello World!</h1>;
 }
 
-export default Home;
+export default memo(Home, isEqual);
