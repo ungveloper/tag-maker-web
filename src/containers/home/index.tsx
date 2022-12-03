0f684badc667b07ed1095b247bc4d9c5c0008aa3
ungveloper <106213837+ungveloper@users.noreply.github.com>
2022-12-03 14:08:21 +0900

    next-i18next 테스트 제거

--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -1,16 +1,9 @@
-import { useRouter } from "next/router";
 import { useTranslation } from "next-i18next";
 import { memo } from "react";
 import isEqual from "react-fast-compare";
 
 function Home(): JSX.Element {
   const { t } = useTranslation("common");
-
-  const { locale, locales, defaultLocale } = useRouter();
-
-  console.log("locale: ", locale);
-  console.log("locales: ", locales);
-  console.log("defaultLocale: ", defaultLocale);
 
   return <h1>{t("title")}</h1>;
 }
