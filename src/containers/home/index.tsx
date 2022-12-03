d4db0d126a3824825b65bc65b3860002fd734861
ungveloper <106213837+ungveloper@users.noreply.github.com>
2022-12-03 14:02:42 +0900

    next-i18next 추가

--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -1,8 +1,18 @@
+import { useRouter } from "next/router";
+import { useTranslation } from "next-i18next";
 import { memo } from "react";
 import isEqual from "react-fast-compare";
 
 function Home(): JSX.Element {
-  return <h1>Hello World!</h1>;
+  const { t } = useTranslation("common");
+
+  const { locale, locales, defaultLocale } = useRouter();
+
+  console.log("locale: ", locale);
+  console.log("locales: ", locales);
+  console.log("defaultLocale: ", defaultLocale);
+
+  return <h1>{t("title")}</h1>;
 }
 
 export default memo(Home, isEqual);
